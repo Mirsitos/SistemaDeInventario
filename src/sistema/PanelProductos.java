07c385ffeb0110a66236a71f47233e632a5c218a
Troopersito <alejandrozelfy751@gmail.com>
2023-11-12 21:20:56 -0300

    Panel nuevo producto

--- src/sistema/PanelProductos.java
+++ src/sistema/PanelProductos.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
 import javax.swing.UIManager;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class PanelProductos extends JPanel {
 
@@ -16,6 +18,12 @@ public class PanelProductos extends JPanel {
 		setBackground(Color.DARK_GRAY);
 		setBounds(0, 0, 630,388);
 		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("NUEVO PRODUCTO");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBounds(234, 11, 139, 14);
+		add(lblNewLabel);
 		setVisible(true);
 	}
 
